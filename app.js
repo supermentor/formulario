03e357d4b46d3eb192316d72de5359c5b6d3c28e
Artur Barbosa <arturbarbosa10@gmail.com>
2019-08-04 18:04:01 -0300

    added a bunch of things

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
 var express = require('express');
 var app = express();
+var credentials = require('./credentials.js');
+
+var handlebars = require('express-handlebars').create({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    partialsDir: __dirname + '/views/partials/'
+});
+
+//Set up view engine (Handlebars)
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
 
 app.set('port', process.env.PORT || 3000);
+
+//Static Files
+app.use(express.static(__dirname + '/public'));
+
+//Body Parser
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
 
 //Routes
 app.get('/', (req, res) =>{
     res.render('home');
 });
-app.use(function(res, req, next){
-    res.type('text/plain');
-    res.status(500);
-    res.send("500 - Server Error");
-});
-app.use(function(res, req, next){
-    res.type('text/plain');
-    res.status(404);
-    res.send("404 - Not Found");
+app.post('/process', (req, res) => {
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        console.log('Form (from querystring): ' + req.query.form);
+        console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+        console.log('Name (from visible form field): ' + req.body.name);
+        console.log('Email (from visible form field): ' + req.body.email);
+        res.send({ success: true, name: req.body.name });
+    } else {
+        // if there were an error, we would redirect to an error page
+        
+    }
 });
 
 app.listen(app.get('port'), function(){
